0c097369edc33e3352deae79a171bef290635727
avkritsky <fon-fan1@yandex.ru>
2022-12-27 20:18:57 +0300

    union record and logger

--- models/logger.py
+++ models/logger.py
@@ -1,7 +1,30 @@
 from dataclasses import dataclass
 from typing import Optional
+from enum import Enum
 
-from models import record
+
+class Level(Enum):
+    """
+    DEBUG = 4
+    WARNING = 3
+    INFO = 2
+    CRITICAL = 1
+    ERROR = 0
+    """
+    DEBUG = 4
+    WARNING = 3
+    INFO = 2
+    CRITICAL = 1
+    ERROR = 0
+
+
+@dataclass(frozen=True)
+class Record:
+    user: str
+    project: str
+    ref: str
+    level: int
+    mess: str
 
 
 class Logger:
@@ -14,49 +37,49 @@ class Logger:
             self.user = user
             self.project = project
             self.ref = ref
-            self.last_log: Optional[record.Record] = None
+            self.last_log: Optional[Record] = None
             self.level = level
             self.__class__._instance[ref] = self
 
     def debug(self, mess: str) -> bool:
-        if self.level < record.Level.DEBUG.value:
+        if self.level < Level.DEBUG.value:
             return False
-        self._create_record(mess, record.Level.DEBUG.value)
+        self._create_record(mess, Level.DEBUG.value)
         return True
 
     def warning(self, mess: str) -> bool:
-        if self.level < record.Level.WARNING.value:
+        if self.level < Level.WARNING.value:
             return False
-        self._create_record(mess, record.Level.WARNING.value)
+        self._create_record(mess, Level.WARNING.value)
         return True
 
     def info(self, mess: str) -> bool:
-        if self.level < record.Level.INFO.value:
+        if self.level < Level.INFO.value:
             return False
-        self._create_record(mess, record.Level.INFO.value)
+        self._create_record(mess, Level.INFO.value)
         return True
 
     def critical(self, mess: str) -> bool:
-        if self.level < record.Level.CRITICAL.value:
+        if self.level < Level.CRITICAL.value:
             return False
-        self._create_record(mess, record.Level.CRITICAL.value)
+        self._create_record(mess, Level.CRITICAL.value)
         return True
 
     def error(self, mess: str) -> bool:
-        if self.level < record.Level.ERROR.value:
+        if self.level < Level.ERROR.value:
             return False
-        self._create_record(mess, record.Level.ERROR.value)
+        self._create_record(mess, Level.ERROR.value)
         return True
 
     def _create_record(self, mess: str, level: int):
         if not isinstance(mess, str):
             raise NotValidMessage('Your message not STR!')
 
-        new_record = record.Record(user=self.user,
-                                   project=self.project,
-                                   ref=self.ref,
-                                   level=level,
-                                   mess=mess)
+        new_record = Record(user=self.user,
+                            project=self.project,
+                            ref=self.ref,
+                            level=level,
+                            mess=mess)
         self.last_log = new_record
         self._save_log()
 
--- models/record.py
+++ models/record.py
@@ -1,26 +1,4 @@
-from dataclasses import dataclass
-from enum import Enum
 
 
-class Level(Enum):
-    """
-    DEBUG = 4
-    WARNING = 3
-    INFO = 2
-    CRITICAL = 1
-    ERROR = 0
-    """
-    DEBUG = 4
-    WARNING = 3
-    INFO = 2
-    CRITICAL = 1
-    ERROR = 0
 
 
-@dataclass(frozen=True)
-class Record:
-    user: str
-    project: str
-    ref: str
-    level: int
-    mess: str
